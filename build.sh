cecb288f26a3bba83a798994c3e53ccd1a89b61d
Chris Pearce <chris.pearce@five.ai>
2020-01-10 23:01:32 +0000

    Fix build

--- build.sh
+++ build.sh
@@ -3,13 +3,11 @@
 echo "Building from ./src/ into ./docs/"
 
 # setup containing directories
-rm -rf docs/
+rm -rf ./docs/
 mkdir docs
-mkdir docs/assets
-mkdir docs/blog
 
 # copy all base files
-cp -rf ./src/ ./docs/
+cp -rf ./src/. ./docs/
 
 # minify html
 html-minifier --input-dir ./src/ --output-dir ./docs/ --file-ext html --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css
